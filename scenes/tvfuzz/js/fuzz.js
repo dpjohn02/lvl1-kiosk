acfcd137839c5d2b66f10cf8f5f2f8fe3e514453
David Jokinen <jokinen.d@gmail.com>
2015-06-07 15:40:15 -0400

    Added a new commercial named TV-Fuzz

--- scenes/tvfuzz/js/fuzz.js
+++ scenes/tvfuzz/js/fuzz.js
@@ -7,24 +7,22 @@ function fuzz(){
 	var imgLoc = [];
 	var screen = null;
 	
-	var letterS = [1,0, 2,0, 3,0, 0,1, 0,2, 1,2, 2,2, 3,2, 3,3,   2,4, 1,4, 0,4];
-	var letterP = [0,0, 1,0, 2,0, 0,1, 0,2, 1,2,2,2, 2,1, 0,3,0,4];
-	var letterE = [0,0, 1,0, 2,0, 0,1, 0,2, 1,2, 0,3,0,4,1,4,2,4];
-	var letterD = [0,0, 1,0,2,0,  0,1, 0,2, 0,3,0,4,1,4 ,2,4 ,3,1,3,2,3,3];
+	var letterL = [0,0, 0,1, 0,4, 1,4,2,4, 0,2, 0,3,0,4];
+	var letterV = [0,0, 0,1, 0,2, 0,3, 0,4, 1,4, 2,3, 2,2, 2,1, 2,0];
+	var letter1 = [0,1, 1,0, 1,1,  1,2, 1,3, 0,4,1,4,2,4];
 	
-	var letterA = [0,0, 1,0, 2,0, 3,0,2,2, 0,1, 0,2, 1,2, 0,3, 0,4, 3,4,3,1,3,2,3,3];
-	var letterC = [0,0, 1,0, 2,0, 0,1, 0,2, 0,3,0,4,1,4,2,4];
-	var letterM = [0,0, 2,0,1,0, 0,2, 0,1, 2,1,2,2,2,3,2,4, 4,4,  0,3, 0,4 ,3,0 ,4,1 ,4,2, 4,3, 4,4, 4,4];
 	var R1 = 200;
 	var G1 = 200;
 	var B1 = 200;
-	var R2 = 138;
-	var G2 = 181;
+	var R2 = 255;
+	var G2 = 255;
 	var B2 = 255;
-	var S1 = Math.random()*500+500;
-	var S2 = Math.random()*500+500;
-	var S3 = Math.random()*500+500;
-	var t = Date.now();
+	var S1 = Math.random()*50000+500;
+	var S2 = Math.random()*50000+500;
+	var S3 = Math.random()*50000+500;
+	var cx = 1;
+	var cy = 1;
+	var t = Date.now()+Math.random()*50000;
 	function ranA(){return Math.random()-.5;} 
 		var ar = 1+ranA(), ag = 2+ranA(), ab = 2+ranA();
 	
@@ -44,6 +42,7 @@ function fuzz(){
 
 	function render(ctx){
 		Color2 = '#A0C8FF';	
+
 		function c(num,a) 
 				{return ('0' + Math.floor((num-(num/3))+Math.sin((t*a)*6.28/18000) *(num/3)).toString(16)).substr(-2);}
 			
@@ -53,11 +52,11 @@ function fuzz(){
 				if(letter[q]==x&&letter[q+1]==y) return 80-50*Math.floor(Math.sin(((t*ar+t*ag+t*ab)/3-1)*6.28/18000)); 
 			return 0;} 
 		function a(num,x,y) 
-			{	return num;
-			
+			{	
+				return l(letterL,x-1-cx,y-1-cy) +l(letterV,x-5-cx,y-1-cy)+l(letterL,x-9-cx,y-1-cy)+l(letter1,x-13-cx,y-1-cy)+ num;
 			}
 		function m(num,x,y) 
-			{return num - 4+4*Math.sin(12  +(t/S1-y*Math.cos(t/4300)+Math.floor(x*Math.tan(t/4100))))
+			{return num - 4+4*Math.sin(12  +(t/S1-y*Math.cos(t/3300)+Math.floor(x*Math.tan(t/4100))))
 						 - 4+4*Math.cos(12  +(t/S1-Math.floor(y*Math.tan(t/4300))+x*Math.sin(t/4100)))
 						//- 4+4*Math.cos(152 +(t/S2-y*Math.cos(t/4300)+x*Math.sin(t/1200)))//;}
 						- 2+Math.floor(2*Math.atan(234 +(t/15-y/x)));}
@@ -65,6 +64,8 @@ function fuzz(){
 			{return '#' + c(a(m(R1,x,y),x,y),ar) + c(a(m(G1,x,y),x,y),ag) + c(a(m(B1,x,y),x,y),ab);}	
 		
 		pixelSize = 30;
+		if(Math.random()>.99)
+			cx = ~~((ctx.width/pixelSize-16)*Math.random())+1,cy = ~~((ctx.height/pixelSize-6)*Math.random())+1;
 		for(var x = 1;x<ctx.width/pixelSize+1;x++)
 			for(var y = 1;y<ctx.height/pixelSize+1;y++)
 			{
@@ -75,7 +76,7 @@ function fuzz(){
 	}
 
 	function update(delta){
-		t += delta;
+		t += delta/2;
 
 	}
 	
